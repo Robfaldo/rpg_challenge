ab1b0542c9c4c78eac907d4fed4b5d39e0d6d8eb
Robert Faldo <robertfaldo@gmail.com>
2018-05-31 21:39:40 +0100

    Added player 2 hitpoints

--- app.rb
+++ app.rb
@@ -1,7 +1,9 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  STARTING_HEALTH = 100
   enable :sessions
+
   get '/' do
     erb :index
   end
@@ -9,12 +11,14 @@ class Battle < Sinatra::Base
   post '/names' do
     session['player_1'] = params[:player_1_name]
     session['player_2'] = params[:player_2_name]
+    session['player_2_hitpoints'] = STARTING_HEALTH
     redirect "/play"
   end
 
   get '/play' do
     @player_1 = session['player_1']
     @player_2 = session['player_2']
+    @player_2_hitpoints = session['player_2_hitpoints']
     erb :play
   end
 
--- spec/features/enter_names_spec.rb
+++ spec/features/enter_names_spec.rb
@@ -1,9 +1,6 @@
-# feature 'Testing infrastructure' do
-#   scenario 'Can run app and check page content' do
-#     visit("/")
-#     expect(page).to have_content 'Testing infrastructure working!'
-#   end
-# end
+# As two Players,
+# So we can play a personalised game of Battle,
+# We want to Start a fight by entering our Names and seeing them
 
 feature 'start flight' do
 	scenario 'print player name' do
@@ -11,7 +8,6 @@ feature 'start flight' do
 		fill_in('player_1_name',with: 'player 1')
 		fill_in('player_2_name',with: 'player 2')
 		click_button('Submit')
-
 		expect(page).to have_content 'player 1 vs player 2'
 	end
 end
