96f12058d870a88613e624af2d882648c078ddc7
Robert Faldo <robertfaldo@gmail.com>
2018-06-01 10:33:36 +0100

    got hitpoints to reduce when attackeD

--- app.rb
+++ app.rb
@@ -1,8 +1,7 @@
 require 'sinatra/base'
-require 'player'
+require './lib/player'
 
 class Battle < Sinatra::Base
-  STARTING_HEALTH = 100
   enable :sessions
 
   get '/' do
@@ -12,21 +11,20 @@ class Battle < Sinatra::Base
   post '/names' do
     $player_1 = Player.new(params[:player_1_name])
     $player_2 = Player.new(params[:player_2_name])
-    session['player_2_hitpoints'] = STARTING_HEALTH
     redirect "/play"
   end
 
   get '/play' do
-    @player_1 = $player_1.name
-    @player_2 = $player_2.name
-    @player_2_hitpoints = session['player_2_hitpoints']
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @player_2_hitpoints = $player_2.hp
     erb :play
   end
 
   post '/attack' do
-    @player_1 = $player_1.name
-    @player_2 = $player_2.name
-    @player_2_hitpoints = session['player_2_hitpoints']
+    @player_1 = $player_1
+    @player_2 = $player_2
+    @player_1.attack(@player_2)
     @attacked = true
     erb :play
   end
--- spec/features/view_hitpoints_spec.rb
+++ spec/features/view_hitpoints_spec.rb
@@ -1,12 +1,23 @@
 
-# As Player 1,
-# So I can see how close I am to winning
-# I want to see Player 2's Hit Points
+feature 'Hitpoints: ' do
 
-feature 'see hitpoints' do
+  # As Player 1,
+  # So I can see how close I am to winning
+  # I want to see Player 2's Hit Points
   scenario 'player 2s hitpoints are visible' do
     sign_in_and_play
     expect(find('progress')['max']).to eq "100"
   end
 
+  # As Player 1,
+  # So I can start to win a game of Battle,
+  # I want my attack to reduce Player 2's HP by 10
+  scenario 'When p1 attacks p2, p2s health reduces by 10' do
+    # Setup
+    sign_in_and_play
+    # Exercise
+    click_button('ATTACK!')
+    # Verification
+    expect(find('progress')['value']).to eq "90"
+  end
 end
